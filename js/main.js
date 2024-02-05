4bfa42564bdf50beefafb18b6a70e6ca726bc568
Ali Haciyev <heli25851@gmail.com>
2024-02-05 23:38:40 +0400

    second commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,22 @@ $(document).ready(function(){
 
     items:1,
     loop:true,
-    smartSpeed:600
+    smartSpeed:600,
+    margin:3,
 
   });
+
+
+$('.slider-next').click(function() {
+    $(".header-slider").trigger('next.owl.carousel');
+});
+
+$('.slider-prev').click(function() {
+    $(".header-slider").trigger('next.owl.carousel');
+})
+
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
+});
+
 });
